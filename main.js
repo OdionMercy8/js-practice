dfe99adcc4d86bdd8da84a09f77b62d6ff0c5e87
odionMercy8 <oseiweifebhor@gmail.com>
2022-02-21 06:19:34 +0100

    index.html

--- main.js
+++ main.js
@@ -500,3 +500,18 @@ function nextPlayer() {
 
         document.querySelector('.dice').style.display = 'none';
 }
+
+
+document.querySelector('.btn-new').addEventListener('click',function() {
+    scores =[ 0, 0];
+    roundScore =0;
+    activePlayer = 0;
+
+
+    document.querySelector('.dice').style.display='none';
+
+    document.getElementById('score-0').textContent ='0';
+    document.getElementById('score-1').textContent ='0';
+    document.getElementById('current-0').textContent ='0';
+    document.getElementById('current-1').textContent ='0';
+})
